d7df14825ae9711851ff15aaf9ff39f9c6146bc1
joebinns <hello@joebinns.com>
2023-11-11 13:34:40 +0100

    Remove unused detection state enum

--- GameplayMathematics/Source/GameplayMathematics/DetectorActor.h
+++ GameplayMathematics/Source/GameplayMathematics/DetectorActor.h
@@ -11,14 +11,6 @@
 
 class USpotLightComponent;
 
-UENUM(BlueprintType)
-enum class EDetectionState : uint8
-{
-	DE_Neutral UMETA(DisplayName="Neutral"),
-	DE_Warning UMETA(DisplayName="Warning"),
-	DE_Spotted UMETA(DisplayName="Spotted")
-};
-
 UCLASS()
 class GAMEPLAYMATHEMATICS_API ADetectorActor : public AActor
 {
@@ -54,7 +46,6 @@ private:
 	UPROPERTY()
 	APawn* Player;
 	
-	EDetectionState DetectionState = EDetectionState::DE_Neutral;
 	float TimeInCone = 0.f;
 
 };
