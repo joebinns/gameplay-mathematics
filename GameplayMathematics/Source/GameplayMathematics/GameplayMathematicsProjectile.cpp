da856ec7fce9f0c2056a62e8702aca07d5377aeb
joebinns <hello@joebinns.com>
2024-01-05 11:39:20 +0100

    Fix incomplete comment

--- GameplayMathematics/Source/GameplayMathematics/GameplayMathematicsProjectile.cpp
+++ GameplayMathematics/Source/GameplayMathematics/GameplayMathematicsProjectile.cpp
@@ -33,7 +33,7 @@ AGameplayMathematicsProjectile::AGameplayMathematicsProjectile()
 
 void AGameplayMathematicsProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
-	// Only add impulse and destroy projectile if we hit a physics
+	// Only add impulse and destroy projectile if another primitive component is hit that is simulating physics
 	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && OtherComp->IsSimulatingPhysics())
 	{
 		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
